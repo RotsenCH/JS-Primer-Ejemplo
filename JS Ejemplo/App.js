e5c047ba903526a497018e881933a4c4f06eb21b
RotsenCH <117753370+RotsenCH@users.noreply.github.com>
2023-02-28 10:00:00 -0500

    Add files via upload

---
+++ JS Ejemplo/App.js
@@ -0,0 +1,13 @@
+document.write("<h1> Hola Mundo XD </h1>");
+console.log("Mensaje en consola");
+
+let nombre = "Néstor";
+
+nombre = "David";
+
+console.log(nombre);
+const PI = 3.1415;
+
+PI = 100;
+
+console.log(PI);
